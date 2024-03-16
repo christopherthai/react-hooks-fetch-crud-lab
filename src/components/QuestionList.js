a73b4478e7904db22a48024f2bf2bb63c96cdb09
Christopher Thai <christopherthai.nyli@gmail.com>
2024-03-15 22:39:01 -0400

    complete lab

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,65 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+
+  const [questions, setQuestions] = useState([]);
+
+  // Use the useEffect hook to fetch the questions from the server
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((response) => response.json())
+      .then((questions) => setQuestions(questions));
+  }, []); // The empty array ensures that the effect runs only once
+
+  // Define a function to handle the delete click event
+  const handleDeleteClick = (id) => {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE",
+    })
+      .then((response) => response.json())
+      .then((question_data) => {
+        const updatedQuestions = questions.filter((question) => question.id !== id);
+        setQuestions(updatedQuestions);
+      });
+  }
+
+  // Define a function to handle the answer change event
+  const handleAnswerChange = (id, correctIndex) => {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ correctIndex }),
+    })
+      .then((response) => response.json())
+      .then((question_data) => {
+        const updatedQuestions = questions.map((question) => {
+          if (question.id === id) {
+            return question_data;
+          } else {
+            return question;
+          }
+        });
+        setQuestions(updatedQuestions);
+      });
+  }
+
+  // Map over the questions and render a QuestionItem component for each question
+  const questionItems = questions.map((question) => (
+    <QuestionItem
+      key={question.id}
+      question={question}
+      onDeleteClick={handleDeleteClick}
+      onAnswerChange={handleAnswerChange}
+    />
+  ));
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{questionItems}</ul>
     </section>
   );
 }
